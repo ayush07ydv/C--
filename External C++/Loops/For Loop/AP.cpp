c9991ef152596569ea42c0ba24b7741dd7ad95ab
Ayush Kumar Yadav <ayush07ydv@gmail.com>
2026-02-15 09:37:38 +0530

    Addes one more method in AP

--- External C++/Loops/For Loop/AP.cpp
+++ External C++/Loops/For Loop/AP.cpp
@@ -12,11 +12,26 @@ int main(){
     }
 
     //Display this AP - 4 7 10 13 16.......
+
+    //Mathematical Method
     int x;
     cout<<"Enter how much you need: ";
     cin>>x;
 
     for(int v=4;v<=3*x+1;v=v+3){
         cout<<v<<endl;
+    }
+
+    //Non-mathematical Method [Need Xtra variable]/* code *//* cod/* code */e */
+    int y, d;
+    cout<<"Enter how much you need: ";
+    cin>>y;
+    cout<<"Start From: ";
+    cin>>d;
+
+    for (int g = 1; g <= y; g++)
+    {
+        cout<<d<<endl;
+         d=d+3;
     }
 }
